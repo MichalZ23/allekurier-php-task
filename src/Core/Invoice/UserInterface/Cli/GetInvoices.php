1ba9bce906a7f21757a9c52b2b76f05bd54e6ebd
Michał Zawalich <mich.zawalich@gmail.com>
2024-11-22 23:38:42 +0100

    Fix incorrect parameters handling in app:invoice:get-by-status-and-amount command

--- src/Core/Invoice/UserInterface/Cli/GetInvoices.php
+++ src/Core/Invoice/UserInterface/Cli/GetInvoices.php
@@ -5,6 +5,8 @@ namespace App\Core\Invoice\UserInterface\Cli;
 use App\Common\Bus\QueryBusInterface;
 use App\Core\Invoice\Application\DTO\InvoiceDTO;
 use App\Core\Invoice\Application\Query\GetInvoicesByStatusAndAmountGreater\GetInvoicesByStatusAndAmountGreaterQuery;
+use App\Core\Invoice\Domain\Exception\InvoiceStatusInvalidException;
+use App\Core\Invoice\Domain\Status\InvoiceStatus;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -24,8 +26,13 @@ class GetInvoices extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $invoiceStatus = InvoiceStatus::tryFrom($input->getArgument('status'));
+        if (!$invoiceStatus) {
+            throw new InvoiceStatusInvalidException('Podany status nie istnieje');
+        }
         $invoices = $this->bus->dispatch(new GetInvoicesByStatusAndAmountGreaterQuery(
-            $input->getArgument('amount')
+            $input->getArgument('amount'),
+            $invoiceStatus,
         ));
 
         /** @var InvoiceDTO $invoice */
